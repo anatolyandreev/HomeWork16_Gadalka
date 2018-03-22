04dcab5253232720bf3319f07f5ef0ff895060f0
anatolyandreev <36275905+anatolyandreev@users.noreply.github.com>
2018-03-22 22:21:55 +0300

    Add files via upload

--- srctst/by/htp/gadalka/Test2.java
+++ srctst/by/htp/gadalka/Test2.java
@@ -13,21 +13,37 @@ public class Test2 {
 	Client cl;
 	Gadanie gadanie;
 	Gadalka gadalka;
+	Romashka romashka;
 	
 	@Before
 	public void initTestData() {
 		cl = new Client();
 		gadalka = new Gadalka();
 		gadanie = new Gadanie();
+		romashka = new Romashka();
 		
 	}
 	
 	@Test
 	public void gadanieTest1() {
-		String actual = cl.askGadalkaPogadat("love", gadalka);
+		String actual = cl.askGadalkaPogadat("work", gadalka);
 		String[] expected = Gadanie.getWorkvariants();
-		System.out.println(cl.askGadalkaPogadat("work", gadalka));
+		assertTrue(Arrays.asList(expected).contains(actual));
 	}
 	
-
+	@Test
+	public void gadanieTest2() {
+		int size = gadalka.getRomashki().get(0).getSize();
+		assertTrue(size > 0);
+	}
+	
+	@Test
+	public void gadanieTest3() {
+		assertTrue(gadalka.getRomashki().size() > 0 && gadalka.getRomashki().size() <= 5 && gadalka.getRomashki() != null);
+	}
+	
+	@Test
+	public void gadanieTest4() {
+		assertTrue(cl.askGadalkaPogadat("work", gadalka) != null);
+	}
 }
